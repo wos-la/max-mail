6db4ed6b05878d030c624f6bacc044cd6dfa56d8
Yang.Zhen <Yang.Zhen@aaa-calif.com>
2020-01-30 15:34:07 -0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace max_mail.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
